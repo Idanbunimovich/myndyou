32ba67085426f142de93bee9d556de5fe0e5bafe
Idan Bunimovich <idanbunimovich@gmail.com>
2023-01-06 19:07:13 +0200

    added class construction

--- api/controllers/controllers.py
+++ api/controllers/controllers.py
@@ -1,11 +1,12 @@
 
 from fastapi import HTTPException
 from data_layer.model.patient import Patient
+import data_layer.models as models
 
 
 def upload(request_body, file):
     try:
-        patients = Patient.convert_csv_data_to_patients(file) if file else Patient.convert_request_data_to_patients(request_body)
+        patients = Patient.convert_csv_data_to_patients(file) if file else Patient(models.Patient).convert_request_data_to_patients(request_body)
         return Patient.save_patients(patients)
     except:
         raise HTTPException(status_code=500, detail="Failed to upload")
@@ -13,7 +14,7 @@ def upload(request_body, file):
 
 def get_patient(id):
     try:
-        patient = Patient.get_patient_by_id(id)
+        patient = Patient(models.Patient).get_patient_by_id(id)
         if not patient:
             raise HTTPException(status_code=404, detail="Patient not found")
         return patient
